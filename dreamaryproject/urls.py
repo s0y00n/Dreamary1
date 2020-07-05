b1d7ece6a891e5267e0c7e92836f3df84d47e7f7
s0y00n <leeesoyoon@gmail.com>
2020-07-05 17:59:39 +0900

    3강

--- dreamaryproject/urls.py
+++ dreamaryproject/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from page import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"), #path는 : 어떤주소/어떤함수 실행/ 뭐라고 부를지) 지정하는 것
-]
+    path('introduce/', views.introduce, name="introduce"),
+    path('new/', views.new, name="new",),
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- page/views.py
+++ page/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def introduce(request):
+    return render(request, 'introduce.html')
+
+def new(request):
+    return render(request, 'new.html')
