fab64f911da7a34d0f534de9d4b78f3c9aeea41b
s0y00n <leeesoyoon@gmail.com>
2020-07-07 02:13:51 +0900

    사진이 안뜸..하..

--- page/views.py
+++ page/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from .models import Designer
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    designers = Designer.objects.all()
+    return render(request, 'home.html', {'designers' : designers})
 
 def introduce(request):
     return render(request, 'introduce.html')
 
 def new(request):
     return render(request, 'new.html')
+
