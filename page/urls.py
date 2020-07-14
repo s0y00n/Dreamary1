d5962c7c9320a3962f480a8f8745810568970a7d
s0y00n <leeesoyoon@gmail.com>
2020-07-14 14:20:41 +0900

    강의 끝!

--- page/urls.py
+++ page/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[
-    path('', views.home, name="home"), #path는 : 어떤주소/어떤함수 실행/ 뭐라고 부를지) 지정하는 것
-    path('introduce/', views.introduce, name="introduce"),
-    path('profile/<int:designer_id>', views.detail, name="detail"),
+urlpatterns = [
+    path('', views.home, name = "home"),
+    path('introduce/', views.introduce, name = "introduce"),
+    path('profile/<int:designer_id>/', views.detail, name = "detail"),
+    path('new/', views.new, name = "new"),
+    path('create/', views.create, name = "create"),
+    path('update/<int:designer_id>/', views.update, name = "update"),
+    path('delete/<int:designer_id>/', views.delete, name = "delete"),
 ]
